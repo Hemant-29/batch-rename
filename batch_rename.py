59f935399113616ccb5405938a2ddeebf4a2cca2
Hemant <68590578+Hemant-29@users.noreply.github.com>
2023-06-09 20:48:43 +0530

    updated main file

--- batch_rename.py
+++ batch_rename.py
@@ -2,20 +2,17 @@ import os
 
 filename_first = input("First file name: ")
 filename_last = input("Last file name: ")
+changed_first = input("Enter first number of the renamed files: ")
 decreasing = input("Want to rename in decreasing order? : ")
-
-if decreasing != None:
-    if decreasing.lower() in ["yes", "y"]:
-        changed_first = input("enter ending number: ")
-    else:
-        changed_first = input("enter starting number: ")
-else:
-    changed_first = input("enter starting number: ")
 
 textfile = open("_cmd.cmd", 'w')
 textfile.write("")
 textfile.close()
+
 numlength = 0
+for i in filename_first:
+    if i.isnumeric():
+        numlength += 1
 
 
 def get_extension(filename):
@@ -41,12 +38,10 @@ def extract_string(filename, number=""):
 
 
 def extract_num(filename):
-    global numlength
     filenumber = ""
     for i in filename:
         if i.isnumeric():
             filenumber += i
-    numlength = len(filenumber)
     return int(filenumber)
 
 
@@ -54,8 +49,10 @@ def generate_command(filename_first, filename_last):
     global numlength, changed_first
     input_list = []
     output_list = []
+
     if extract_num(filename_first) > extract_num(filename_last):
-        print("wrong input!\nThe second number must be larger")
+        print("wrong input! The second number must be larger")
+        return
 
     for i in range(extract_num(filename_first), extract_num(filename_last)+1):
         input_list.append(str(i).zfill(numlength))
@@ -64,11 +61,12 @@ def generate_command(filename_first, filename_last):
     changed_last = f"{len(input_list)+extract_num(changed_first)}" + \
         get_extension(filename_first)
 
-    if decreasing in ["no", "NO", "No", "n", "N", None]:
-        for i in range(extract_num(changed_first), extract_num(changed_last)+1):
+    if decreasing in ["no", "NO", "No", "n", "N", ""] or decreasing is None:
+        for i in range(extract_num(changed_first), extract_num(changed_last)):
             output_list.append(str(i))
+
     if decreasing.lower() in ["yes", 'y']:
-        for i in range(extract_num(changed_last), extract_num(changed_first)-1, -1):
+        for i in range(extract_num(changed_last)-1, extract_num(changed_first)-1, -1):
             output_list.append(str(i))
 
     for n, i in enumerate(input_list):
